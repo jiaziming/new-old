85c37571793ef920f04dd13e9fcf6f743d0c23b4
jiaziming <jiaziming123@sina.cn>
2019-03-11 11:40:44 +0800

    数据库操作

--- day16/App01/models.py
+++ day16/App01/models.py
@@ -6,15 +6,19 @@ class Author(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     email = models.EmailField()
+    def __str__(self):
+        return "<%s %s>" %(self.first_name,self.last_name)
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=64,unique=True)
     address = models.CharField(max_length=128)
     city = models.CharField(max_length=64)
-    state_province = models.CharField(max_length=30)
+    state_province = models.CharField(max_length=30,help_text="请正确填写内容",verbose_name="所属省份")
     country = models.CharField(max_length=50)
     website = models.URLField()
+    def __str__(self):
+        return "<%s>" %(self.name)
 
 
 class Book(models.Model):
@@ -22,3 +26,5 @@ class Book(models.Model):
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher,on_delete=models.CASCADE,)
     publish_date = models.DateField()
+    def __str__(self):
+        return "<%s>" %(self.name)
--- day16/day16/settings.py
+++ day16/day16/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App01.apps.App01Config',
+    'App01',
 ]
 
 MIDDLEWARE = [
