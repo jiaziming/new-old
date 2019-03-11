4487e69f136d733fb6a5ec4309c09a0fb3b34d0b
jiaziming <jiaziming123@sina.cn>
2019-03-11 14:30:50 +0800

    django/admin 更新

--- day16/App01/admin.py
+++ day16/App01/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from App01 import models
+
+admin.site.register(models.Author)
+admin.site.register(models.Publisher)
+admin.site.register(models.Book)
