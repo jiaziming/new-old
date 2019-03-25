823bd84cb1bb23c0112c7e4072720f0335058857
jiaziming <jiaziming123@sina.cn>
2019-03-25 18:54:51 +0800

    django/admin

--- day16/App01/admin.py
+++ day16/App01/admin.py
@@ -4,6 +4,18 @@ from django.contrib import admin
 
 from App01 import models
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id','name','publisher','publish_date')
+    search_fields = ('name','publisher__name')
+    list_filter = ('publisher','publish_date')
+    list_editable = ('name','publish_date')
+    list_per_page = 10
+
+    filter_horizontal = ('authors',)    #for m 2 m
+    raw_id_fields = ('publisher',)      #for fk
+
+
 admin.site.register(models.Author)
 admin.site.register(models.Publisher)
-admin.site.register(models.Book)
+admin.site.register(models.Book,BookAdmin)
+
